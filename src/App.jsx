ddcfe1c63486dd75cc97379a4411d25325256fa6
‘Katrin <katrin.baier77@gmail.com>
2022-09-30 14:42:22 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,16 @@ function App() {
   return (
     <>
       <div className="container text-left text-lg text-slate-800">
-        <h1 className="text-5xl">Hacker News</h1>
-        <NewsFetch {...kitchenSink} />
-        <NewsPage {...kitchenSink} />
-        <Pagination {...kitchenSink} />
+        <header className="py-5 container bg-gradient-to-r from-orange-400 via-orange-400 to-orange-600">
+          <h1 className="py-5 text-5xl text-center text-white uppercase">Hacker News</h1>
+          <NewsFetch {...kitchenSink} />
+        </header>
+        <body className="my-5">
+          <NewsPage {...kitchenSink} />
+        </body>
+        <footer className="text-center">
+          <Pagination {...kitchenSink} />
+        </footer>
       </div>
     </>
   )
